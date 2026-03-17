322c7d23fc576a1b967439d99ba29fbaeee496da
Curve <pawapichaya.c@kkumail.com>
2026-03-18 01:43:01 +0700

    fix express route for render

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
